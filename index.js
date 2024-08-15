33c52ca7cdc2bd136484176f4eedea11f6b1f34c
Paulo Souza <paulo18santos2000@gmail.com>
2024-08-15 11:12:49 -0300

    adicao do nodemon e adicao de uma rota get

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 const app = require('./src/app')
+
 app.listen(9001, () => console.log('Server on in 9001'))
--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res) => {
+    res.json('Bem vindo a aplicação')
+})
 
 module.exports = app
